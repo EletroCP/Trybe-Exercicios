44685c62068196cf6c2d0f20629a0595736baf89
Breno L. Lopes <brernolopes@hotmail.com>
2022-02-16 18:55:08 -0300

    Exercicios 1 e 2

--- first-interaction-with-react/src/App.js
+++ first-interaction-with-react/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+const compromissos = ['estudar react', 'rever a aula', 'fazer os exercicios do dia'];
+
+const Task = (value) => {
+  return (
+    <li key={value}>{value}</li>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Hello World!</p>
+      <ul>{ compromissos.map((element,) => Task(element)) }</ul>
     </div>
   );
 }
